b90215dcfc4d7b5b9408ddf19c1d9d9459d71e50
Joe Blackett <joseph.blackett@gmail.com>
2016-02-14 14:21:04 -0500

    creating templating function

---
+++ client/templates/posts/post_item.js
@@ -0,0 +1,8 @@
+/* global Template */
+Template.postItem.helpers({
+    domain: function() {
+        var a = document.createElement('a');
+        a.href = this.url;
+        return a.hostname;
+    } 
+});
